cc8e1760aff483428aa7cc32fc7172ff857e85d2
gray <gray@cpan.org>
2010-06-15 03:35:21 -0700

    update copyright dates

--- lib/Geo/Coder/Multimap.pm
+++ lib/Geo/Coder/Multimap.pm
@@ -190,7 +190,7 @@ L<http://search.cpan.org/dist/Geo-Coder-Multimap>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2009 gray <gray at cpan.org>, all rights reserved.
+Copyright (C) 2010 gray <gray at cpan.org>, all rights reserved.
 
 This library is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
